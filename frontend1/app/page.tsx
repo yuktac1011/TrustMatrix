7f7fa9c586280415e4b7eafa7c819c04567f34bf
yuktac1011 <chaudhariyukta936@gmail.com>
2026-07-26 11:24:52 +0530

    ci: add GitHub Actions workflow for frontend build checks

--- frontend1/app/page.tsx
+++ frontend1/app/page.tsx
@@ -63,6 +63,8 @@ export default function LandingPage() {
           mouseStrength={0.3}
           pageLoadAnimation={false}
           brightness={0.8}
+          className=""
+          style={{}}
         />
       </div>
 
